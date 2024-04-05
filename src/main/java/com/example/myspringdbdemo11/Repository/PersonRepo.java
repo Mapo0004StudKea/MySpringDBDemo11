ab8f00b61dae9b311324bc639c1cb6bc4c10a1cc
Martin Poulsen <mapo0004@stud.kea.dk>
2024-04-05 10:55:50 +0200

    added comment

--- src/main/java/com/example/myspringdbdemo11/Repository/PersonRepo.java
+++ src/main/java/com/example/myspringdbdemo11/Repository/PersonRepo.java
@@ -35,9 +35,5 @@ public class PersonRepo {
         String sql = "UPDATE person SET first_name = ?, last_name = ? WHERE id = ?";
         template.update(sql, p.getFirst_name(), p.getLast_name(), p.getId());
     }
-
-    //comment
-    /**
-     * asdasdasdasd
-     */
+    
 }
